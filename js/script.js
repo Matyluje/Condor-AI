7ad331ea797c0d2e8ebdf7b7fe848c6accbddee3
Matias Luje <luje.matias11@gmail.com>
2025-08-04 17:56:41 -0500

    Pagina principal y Tu agente completa

---
+++ js/script.js
@@ -0,0 +1,5 @@
+console.log("Condor Assist AI está vivo 🦅");
+
+document.getElementById("cta").addEventListener("click", () => {
+  alert("Gracias por tu interés. ¡Pronto podrás agendar tu demo!");
+});
